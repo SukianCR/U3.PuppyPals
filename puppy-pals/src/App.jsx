1054084267184e87d1ab2ca2e2856d6c4ddc7acf
sukimixcr <sukimixcr@gmail.com>
2024-05-28 21:43:18 -0700

    adding cards info

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -4,9 +4,12 @@ import { useState } from 'react';
 
 function App() {
   // console.log(puppyList);
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
   //console.log(puppies);
   const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+  
 /*
   function handleClick() {
     // some logic here
@@ -16,7 +19,15 @@ function App() {
   return (
     <div className="App">
 
-      { featPupId && <p>{ featPupId }</p> } 
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      ) } 
 
       {puppies.map((puppy) => {
 
