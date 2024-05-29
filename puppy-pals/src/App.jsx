f0e4addc58aef05cf1e2d7a031f47977fd8c6d81
sukimixcr <sukimixcr@gmail.com>
2024-05-28 20:42:11 -0700

    printing puppys names

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,15 +1,22 @@
+import {puppyList} from './data.js';
+import { useState } from 'react';
+
 
 function App() {
-
-
-  return (
-    <>
-      <div>
-       
-
+ // console.log(puppyList);
+ const [puppies, setPuppies] = useState(puppyList)
+ console.log(puppies);
+  
+ return (
+   
+      <div class="App">
+            { 
+                 puppies.map((puppy) => {
+                 return <p key={puppy.id}>{puppy.name}</p>
+                   })
+            }
       </div>
      
-    </>
   )
 }
 
